83759f9c521db190d2bf155923dd370030d45256
Eric Berquist <erb74@pitt.edu>
2014-06-15 17:50:27 -0400

    Clean up the utils header wrt the diagonalization routines.

--- utils.hpp
+++ utils.hpp
@@ -1,15 +1,20 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+// from diag.cpp:
+void diag(int nm, int n, double **array, double *e_vals, int matz, double **e_vecs, double toler);
 void tred2(int n, double **a, double *d, double *e, int matz);
 void tqli(int n, double *d, double **z, double *e, int matz, double toler);
-void eigsort(double *d, double **v, int n);
 double *init_array(int size);
-double **init_matrix(int n,int m);
+double **init_matrix(int n, int m);
 void free_matrix(double **array, int size);
+void eigsort(double *d, double **v, int n);
 // void print_mat(double **a, int m, int n, FILE *out);
-void diag(int nm, int n, double **array, double *e_vals, int matz, double **e_vecs, double toler);
+void zero_matrix(double **a, int m, int n);
+void zero_array(double *a, int m);
+void mmult(double **A, int transa, double **B, int transb, double **C, int l, int m, int n);
 
+// from utils.cpp:
 void print_mat(double **mat, int dim_rows, int dim_cols);
 int compound_index_2(int i, int j);
 int compound_index_4(int i, int j, int k, int l);
--- utils.py
+++ utils.py
@@ -20,14 +20,14 @@ def print_mat(mat):
     # first, handle the column labels
     print(" " * 5),
     for i in range(dim_cols):
-        print("{0:12d}".format(i+1)),
+        print("{0:11d}".format(i+1)),
     print("")
     # then, handle the row labels
     for i in range(dim_rows):
         print("{0:5d}".format(i+1)),
         # print the matrix data
         for j in range(dim_cols):
-            print("{0:12.7f}".format(mat[i][j])),
+            print("{0:11.7f}".format(mat[i][j])),
         print("")
 
 def compound_index_2(i,j):
