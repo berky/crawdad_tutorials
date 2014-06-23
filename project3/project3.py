503367198601467dd23d514d3977732a4c00712c
Eric Berquist <erb74@pitt.edu>
2014-06-22 20:06:24 -0400

    Changed the name of most variables in Python #3 (will later do the same for C++).
    Started outline for SCF iteration loop.

--- project3/project3.py
+++ project3/project3.py
@@ -12,70 +12,76 @@ def read_vnn():
     vnn_handle.close()
     return vnn
 
-def read_s_ao():
-    s_ao_handle = open(stub + "s.dat")
-    s_ao_file = s_ao_handle.readlines()
-    s_ao_handle.close()
-    matsize = int(s_ao_file[-1].split()[0])
-    s_ao = np.zeros(shape=(matsize, matsize))
-    for line in s_ao_file:
+def read_s():
+    s_handle = open(stub + "s.dat")
+    s_file = s_handle.readlines()
+    s_handle.close()
+    matsize = int(s_file[-1].split()[0])
+    s = np.zeros(shape=(matsize, matsize))
+    for line in s_file:
         tmp1, tmp2, tmp3 = line.split()
         mu, nu, s_mu_nu = int(tmp1)-1, int(tmp2)-1, float(tmp3)
-        s_ao[mu][nu] = s_ao[nu][mu] = s_mu_nu
-    return s_ao
+        s[mu][nu] = s[nu][mu] = s_mu_nu
+    return s
 
-def read_t_ao():
-    t_ao_handle = open(stub + "t.dat")
-    t_ao_file = t_ao_handle.readlines()
-    t_ao_handle.close()
-    matsize = int(t_ao_file[-1].split()[0])
-    t_ao = np.zeros(shape=(matsize, matsize))
-    for line in t_ao_file:
+def read_t():
+    t_handle = open(stub + "t.dat")
+    t_file = t_handle.readlines()
+    t_handle.close()
+    matsize = int(t_file[-1].split()[0])
+    t = np.zeros(shape=(matsize, matsize))
+    for line in t_file:
         tmp1, tmp2, tmp3 = line.split()
         mu, nu, t_mu_nu = int(tmp1)-1, int(tmp2)-1, float(tmp3)
-        t_ao[mu][nu] = t_ao[nu][mu] = t_mu_nu
-    return t_ao
+        t[mu][nu] = t[nu][mu] = t_mu_nu
+    return t
 
-def read_v_ao():
-    v_ao_handle = open(stub + "v.dat")
-    v_ao_file = v_ao_handle.readlines()
-    v_ao_handle.close()
-    matsize = int(v_ao_file[-1].split()[0])
-    v_ao = np.zeros(shape=(matsize, matsize))
-    for line in v_ao_file:
+def read_v():
+    v_handle = open(stub + "v.dat")
+    v_file = v_handle.readlines()
+    v_handle.close()
+    matsize = int(v_file[-1].split()[0])
+    v = np.zeros(shape=(matsize, matsize))
+    for line in v_file:
         tmp1, tmp2, tmp3 = line.split()
         mu, nu, v_mu_nu = int(tmp1)-1, int(tmp2)-1, float(tmp3)
-        v_ao[mu][nu] = v_ao[nu][mu] = v_mu_nu
-    return v_ao
+        v[mu][nu] = v[nu][mu] = v_mu_nu
+    return v
 
-def read_eri_ao():
-    eri_ao_handle = open(stub + "eri.dat")
-    eri_ao_file = eri_ao_handle.readlines()
-    eri_ao_handle.close()
-    matsize = int(eri_ao_file[-1].split()[0])
+def read_eri():
+    eri_handle = open(stub + "eri.dat")
+    eri_file = eri_handle.readlines()
+    eri_handle.close()
+    matsize = int(eri_file[-1].split()[0])
     # be very inefficient with how we store these for now -- use all
     # four indices
-    eri_ao = np.zeros(shape=(matsize, matsize, matsize, matsize))
-    for line in eri_ao_file:
+    eri = np.zeros(shape=(matsize, matsize, matsize, matsize))
+    for line in eri_file:
         tmp1, tmp2, tmp3, tmp4, tmp5 = line.split()
         mu = int(tmp1)-1
         nu = int(tmp2)-1
         lm = int(tmp3)-1
         sg = int(tmp4)-1
         eri_val = float(tmp5)
-        eri_ao[mu][nu][lm][sg] \
-            = eri_ao[mu][nu][sg][lm] \
-            = eri_ao[nu][mu][lm][sg] \
-            = eri_ao[nu][mu][sg][lm] \
-            = eri_ao[lm][sg][mu][nu] \
-            = eri_ao[lm][sg][nu][mu] \
-            = eri_ao[sg][lm][mu][nu] \
-            = eri_ao[sg][lm][nu][mu] \
+        eri[mu][nu][lm][sg] \
+            = eri[mu][nu][sg][lm] \
+            = eri[nu][mu][lm][sg] \
+            = eri[nu][mu][sg][lm] \
+            = eri[lm][sg][mu][nu] \
+            = eri[lm][sg][nu][mu] \
+            = eri[sg][lm][mu][nu] \
+            = eri[sg][lm][nu][mu] \
             = eri_val
-    return eri_ao
+    return eri
 
-def calc_elec_energy(P, H, F):
-    return np.sum(P*(H+F))
+def calc_elec_energy(D, H, F):
+    return np.sum(D*(H+F))
+
+def build_density(D, C):
+    for mu in range(NBasis):
+        for nu in range(NBasis):
+            for m in range(NOcc):
+                D[mu][nu] += C[mu][m] * C[nu][m]    
 
 def build_fock(F, D, H, ERI):
     NBasis = len(D)
@@ -104,99 +110,100 @@ if __name__ == "__main__":
     ##########################################################################
     # Step #2: One-Electron Integrals
     
-    S_AO = read_s_ao()
-    T_AO = read_t_ao()
-    V_AO = read_v_ao()
+    S = read_s()
+    T = read_t()
+    V = read_v()
 
-    print("AO Overlap Integrals [S_AO]:")
-    print_mat(S_AO)
-    print("AO Kinetic Energy Integrals [T_AO]:")
-    print_mat(T_AO)
-    print("AO Nuclear Attraction Integrals [V_AO]:")
-    print_mat(V_AO)
+    print("AO Overlap Integrals [S]:")
+    print_mat(S)
+    print("AO Kinetic Energy Integrals [T]:")
+    print_mat(T)
+    print("AO Nuclear Attraction Integrals [V]:")
+    print_mat(V)
     
-    H_AO_Core = T_AO + V_AO
-    print("AO Core Hamiltonian [H_AO_Core]:")
-    print_mat(H_AO_Core)
+    H = T + V
+    print("AO Core Hamiltonian [H]:")
+    print_mat(H)
 
     NElec = 10
     NOcc = NElec//2
-    NBasis = S_AO.shape[0]
+    NBasis = S.shape[0]
 
     ##########################################################################
     # Step #3: Two-Electron Integrals
 
-    ERI_AO = read_eri_ao()
+    ERI = read_eri()
 
     ##########################################################################
     # Step #4: Build the Orthogonalization Matrix
 
-    Lam_S_AO, L_S_AO = spl.eigh(S_AO)
-    Lam_S_AO = Lam_S_AO * np.eye(len(Lam_S_AO))
+    Lam_S, L_S = spl.eigh(S)
+    Lam_S = Lam_S * np.eye(len(Lam_S))
     
-    print("matrix of eigenvectors (columns) [L_S_AO]:")
-    print_mat(L_S_AO)
-    print("diagonal matrix of corresponding eigenvalues [Lam_S_AO]:")
-    print_mat(Lam_S_AO)
+    print("matrix of eigenvectors (columns) [L_S]:")
+    print_mat(L_S)
+    print("diagonal matrix of corresponding eigenvalues [Lam_S]:")
+    print_mat(Lam_S)
     
-    Lam_sqrt_inv_AO = np.sqrt(spl.inv(Lam_S_AO))
-    Symm_Orthog = np.dot(L_S_AO, np.dot(Lam_sqrt_inv_AO, L_S_AO.T))
+    Lam_sqrt_inv = np.sqrt(spl.inv(Lam_S))
+    Symm_Orthog = np.dot(L_S, np.dot(Lam_sqrt_inv, L_S.T))
     
-    print("Symmetric Orthogonalization Matrix [S^-1/2]:")
+    print("Symmetric Orthogonalization Matrix [Symm_Orthog]:")
     print_mat(Symm_Orthog)
     
     ##########################################################################
     # Step #5: Build the (Inital) Guess Density
 
-    F_prime_0_AO = np.dot(Symm_Orthog.T, np.dot(H_AO_Core, Symm_Orthog))
+    F_prime = np.dot(Symm_Orthog.T, np.dot(H, Symm_Orthog))
 
-    print("Initial (guess) Fock Matrix [F_prime_0_AO]:")
-    print_mat(F_prime_0_AO)
+    print("Initial (guess) Fock Matrix [F_prime]:")
+    print_mat(F_prime)
 
     # Diagonalize the Fock matrix
 
-    eps_0_AO, C_prime_0_AO = spl.eigh(F_prime_0_AO)
-    eps_0_AO = eps_0_AO * np.eye(len(eps_0_AO))
+    eps, C_prime = spl.eigh(F_prime)
+    eps = eps * np.eye(len(eps))
 
-    print("Initial MO Coefficients [C_prime_0_AO]:")
-    print_mat(C_prime_0_AO)
-    print("Initial Orbital Energies [eps_0_AO]:")
-    print_mat(eps_0_AO)
+    print("Initial MO Coefficients [C_prime]:")
+    print_mat(C_prime)
+    print("Initial Orbital Energies [eps]:")
+    print_mat(eps)
 
     # Transform the eigenvectors into the original (non-orthogonal) AO basis
 
-    C_0_AO = np.dot(Symm_Orthog, C_prime_0_AO)
+    C = np.dot(Symm_Orthog, C_prime)
 
-    print("Initial MO Coefficients (non-orthogonal) [C_0_AO]:")
-    print_mat(C_0_AO)
+    print("Initial MO Coefficients (non-orthogonal) [C]:")
+    print_mat(C)
 
     # Build the density matrix using the occupied MOs
 
-    D_0 = np.zeros((NBasis,NBasis))
+    D = np.zeros((NBasis, NBasis))
+    build_density(D, C)
 
-    for mu in range(NBasis):
-        for nu in range(NBasis):
-            for m in range(NOcc):
-                D_0[mu][nu] += C_0_AO[mu][m] * C_0_AO[nu][m]
-
-    print("Initial Density Matrix [D_0]:")
-    print_mat(D_0)
+    print("Initial Density Matrix [D]:")
+    print_mat(D)
 
     ##########################################################################
     # Step #6: Compute the Initial SCF Energy
 
-    E_elec_0 = calc_elec_energy(D_0, H_AO_Core, H_AO_Core)
-    E_total_0 = E_elec_0 + vnn
+    E_elec_new = calc_elec_energy(D, H, H)
+    E_total = E_elec_new + vnn
 
-    print("Initial Electronic Energy: {0:20.12f}".format(E_elec_0))
-    print("     Initial Total Energy: {0:20.12f}".format(E_total_0))
+    print("Initial Electronic Energy: {0:20.12f}".format(E_elec_new))
+    print("     Initial Total Energy: {0:20.12f}".format(E_total))
 
     ##########################################################################
     # Step #7: Compute the New Fock Matrix
 
-    F_AO = np.empty((NBasis,NBasis))
-    build_fock(F_AO, D_0, H_AO_Core, ERI_AO)
+    F = np.empty((NBasis,NBasis))
 
-    print("First iteration Fock matrix:")
-    print_mat(F_AO)
-    
+    iter = 1
+    max_iter = 100
+
+    while iter < max_iter:
+        build_fock(F, D, H, ERI)
+        if iter == 1:
+            print("First iteration Fock matrix [F]:")
+            print_mat(F)
+        iter += 1
